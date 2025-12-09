7be534cc377fb1779ba264ee8bcacc07f35d86a0
ArtisenGear <snowbellthewolf@gmail.com>
2025-12-08 19:41:03 -0600

    N/S

---
+++ src/main/java/arti/pratice/PraticeDataGenerator.java
@@ -0,0 +1,11 @@
+package arti.pratice;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PraticeDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/arti/pratice/PraticeModClient.java
@@ -0,0 +1,10 @@
+package arti.pratice;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PraticeModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
